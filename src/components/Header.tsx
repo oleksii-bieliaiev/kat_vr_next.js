6ef44fc9d9114bafbb4c44aafb3e290f70a39bc6
Oleksii Bieliaiev <o.bieliaiev@icloud.com>
2024-10-20 22:21:12 +0200

    app fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import React, { forwardRef, useState } from 'react'
-import NavBar from './NavBar'
-import DownBar from './DownBar'
+import React, { useState } from 'react'
+import NavBar from './NavBar';
+import DownBar from './DownBar';
 import Video from './Video';
 import Link from 'next/link';
 
-const Header = () => {
+const Version = () => {
   const [isVideoVisible, setVideoVisible] = useState(false);
 
   const toggleVideo = () => {
@@ -14,55 +14,72 @@ const Header = () => {
   };
 
   return (
-    <section className='relative flex flex-col min-h-screen' id='page-top'>
+    <section className='relative flex flex-col h-screen bg-gradient-to-r from-[#191536] to-[#000000] overflow-hidden'>
       <NavBar />
-      <div className='flex justify-between'>
-        <img
-          src="/images/gadgets_big.svg"
-          alt="Gadgets"
-          className='2xl:top-[55%] 2xl:transform 2xl:-translate-y-1/2 lg:top-20 mb-10 max-sm:mb-0 sm:mb-0 lg:mr-[110px] md:mr-[34px] max-sm:mr-[34px] sm:mr-[34px] md:top-[18vh] max-sm:top-11 sm:top-11 right-0 lg:w-[810px] lg:h-[668px] 2xl:w-[1054px] 2xl:h-[595px] md:w-[494px] md:h-[348px] w-[100vw] h-[30vh] absolute object-fit z-0'
-        />
-        <img
-          src="/images/Group_2.svg"
-          alt="Gadgets"
-          className='hidden lg:block 2xl:top-[42%] 2xl:transform 2xl:-translate-y-1/2 lg:top-48 lg:left-[110px] lg:w-[479px] lg:h-[253px] 2xl:w-[552px] 2xl:h-[316px] pr-10 object-fit absolute z-1'
-        />
-        <img
-          src="/images/Group_2_tab.svg"
-          alt="Gadgets"
-          className='hidden md:block lg:hidden top-[50vh] md:top-[25vh] md:left-[30px] md:w-[483px] md:h-[195px] object-fit absolute'
-        />
-        <img
-          src="/images/New_start_text.svg"
-          alt="Gadgets"
-          className='hidden max-sm:block sm:block md:hidden left-1/2 transform -translate-x-1/2 top-[34vh] sm:w-[38vw] sm:h-[15vh] object-fit absolute '
-        />
-        <img
-          src="/images/Discover_text.svg"
-          alt="Gadgets"
-          className='hidden max-sm:block sm:block md:hidden left-1/2 transform -translate-x-1/2 top-[48vh] max-sm:w-[260px] sm:h-[22vh] object-fit absolute '
-        />
-        <img
-          src="/images/1200.svg"
-          alt="Gadgets"
-          className='hidden max-sm:block sm:block md:hidden left-1/2 transform -translate-x-1/2 top-[66vh] sm:w-[7vw] sm:h-[5vh] max-sm:top-[65vh] object-fit absolute '
-        />
-        <div
-          className='w-[236px] h-[48px] object-fit absolute cursor-pointer
-                     max-sm:left-1/2 max-sm:translate-x-[-170px] max-sm:top-[71vh] 
-                     sm:left-1/2 sm:translate-x-[-170px] sm:top-[71vh] 
-                     md:left-[0px] md:top-[73vh] md:translate-x-0 
-                     lg:left-[147px] lg:top-[64vh] 
-                     2xl:mt-24 2xl:relative 2xl:top-[42vh] 2xl:left-[158px] 
-                     z-1
-                    bg-[url(/images/Default_play.svg)] hover:bg-[url(/images/Button_video_hover.svg)] transition-all duration-300 bg-no-repeat bg-center bg-contain'
-          onClick={toggleVideo}            
-        />
-        <Link href="#contact" passHref scroll={true}>
-        <button className="hidden max-sm:block bg-[#05C2DF] hover:bg-white hover:text-[#05C2DF] text-white py-2 px-4 border border-[#05C2DF] rounded w-[265px] h-[39px] transition-colors duration-300 mr-[34px] max-sm:top-[85vh] max-sm:translate-x-[-135px] max-sm:left-1/2 absolute">
-          Buy Now
-        </button>
-        </Link>
+
+      <div className='grid lg:grid-cols-12 md:grid-cols-6 max-sm:grid-cols-2 lg:pr-[110px] md:pr-[34px] max-sm:pr-[34px] sm:pr-[34px] lg:pl-[110px] md:pl-[34px] max-sm:pl-[34px] sm:pl-[34px] min-h-screen md:place-items-center'>
+
+        <div className='2xl:col-start-1 2xl:col-span-4 lg:col-start-1 lg:col-span-4 md:col-start-1 md:col-span-3 sm:col-start-1 sm:col-span-2 max-sm:col-start-1 max-sm:col-span-2 flex flex-col items-center justify-center 2xl:translate-y-[-9rem] lg:translate-y-[-8rem] md:translate-y-[-2rem] max-sm:translate-y-[-5rem] relative'>
+          <img
+            src="/images/gadgets_small.svg"
+            alt="Image"
+            className='hidden max-sm:block sm:block md:hidden sm:w-[320px] sm:h-[152px] max-sm:w-[320px] max-sm:h-[152px] sm:mt-0 max-sm:mt-20 max-sm:object-cover max-sm:object-center'
+          />
+          <img
+            src="/images/New_start_text.svg"
+            alt="Gadgets"
+            className='hidden max-sm:block sm:block md:hidden'
+          />
+          <img
+            src="/images/Discover_text.svg"
+            alt="Gadgets"
+            className='hidden max-sm:block mt-7 sm:block md:hidden'
+          />
+          <img
+            src="/images/1200.svg"
+            alt="Gadgets"
+            className='hidden max-sm:block mt-3 sm:block md:hidden'
+          />
+
+          <img
+            src="/images/Group_2.svg"
+            alt="Text"
+            className='hidden lg:block object-contain lg:w-[479px] lg:h-[253px] 2xl:w-[562px] 2xl:h-[316px]'
+          />
+
+          <img
+            src="/images/Group_2_tab.svg"
+            alt="Text"
+            className='hidden md:flex lg:hidden object-cover object-left md:w-[483px] md:h-[195px] md:z-1'
+          />
+
+          <div className='relative w-full 2xl:pl-[9%] 2xl:pr-[32%] lg:pl-[5%] lg:pr-[18%] md:pl-0 md:pr-[50%] max-sm:pl-0 max-sm:pr-[30%] sm:pl-0 sm:pr-[18%] 2xl:mt-8 lg:mt-6 md:mt-6 sm:mt-4 max-sm:mt-6 flex md:justify-start max-sm:justify-center sm:justify-center'>
+            <div
+              className='lg:w-[286px] lg:h-[56px] md:w-[236px] md:h-[48px] max-sm:w-[236px] max-sm:h-[48px] sm:w-[236px] sm:h-[48px] cursor-pointer bg-[url(/images/Default_play.svg)] hover:bg-[url(/images/Button_video_hover.svg)] transition-all duration-300 bg-no-repeat bg-center bg-contain'
+              onClick={toggleVideo}
+            />
+          </div>
+
+          <Link href="#contact" passHref scroll={true}>
+            <button className="hidden max-sm:block sm:block md:hidden bg-[#05C2DF] hover:bg-white hover:text-[#05C2DF] text-white sm:mt-5 max-sm:mt-7 sm:mb-10 border border-[#05C2DF] rounded max-w-[265px] w-[80vw] h-[39px] transition-colors duration-300 ">
+              Buy Now
+            </button>
+          </Link>
+        </div>
+
+
+        <div className='2xl:col-start-6 2xl:col-span-7 lg:col-start-6 lg:col-span-7 md:col-start-4 md:col-span-3 max-sm:col-start-1 max-sm:col-span-2 flex md:justify-center lg:translate-y-[-5rem] md:translate-y-[-1rem] md:z-0'>
+          <img
+            src="/images/gadgets_big.svg"
+            alt="Image"
+            className='hidden lg:block lg:w-[710px] lg:h-[388px] 2xl:w-[1054px] 2xl:h-[645px] md:w-[494px] md:h-[348px] lg:object-contain'
+          />
+          <img
+            src="/images/gadgets_medium.svg"
+            alt="Image"
+            className='hidden md:block lg:hidden md:w-[494px] md:h-[288px] md:object-cover md:object-center md:z-0'
+          />
+        </div>
 
       </div>
       <DownBar />
@@ -72,4 +89,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default Version
